ebb7a347b8e25c3c8af5a02ea55743c63094b889
tuilatho <thob1906579@student.ctu.edu.vn>
2023-02-17 19:29:26 +0700

    khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Hello, Vue.js!</h1>
+  </div>
+</template>
+
+<style>
+  .page {
+    max-width: 400px;
+    margin: auto;
+  }
+</style>
